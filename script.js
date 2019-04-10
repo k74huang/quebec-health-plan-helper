e01cf1a04d43dd17fde422796c41f9a595ff2a22
Kai Huang <edwardthetrueblade2678@gmail.com>
2019-04-09 22:51:24 -0400

    bugfixes woo

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ $(document).ready(function()
 		var resString;
 
 		var arrivalDate = new Date($("#arrival").val());
-		arrivalDate.setHours(arrivalDate.getHours() + 4);
+		arrivalDate.setHours(arrivalDate.getHours() + 5);
+		console.log(arrivalDate);
 
 		var regDate = new Date($("#registration").val());
-		regDate.setHours(regDate.getHours() + 4);
+		regDate.setHours(regDate.getHours() + 5);
+		console.log(regDate);
 
 		if(arrivalDate == 'Invalid Date' || regDate == 'Invalid Date')
 		{
@@ -21,19 +23,27 @@ $(document).ready(function()
 		else
 		{
 			var eligibleDate = new Date(arrivalDate);
-			eligibleDate.setMonth(arrivalDate.getMonth() + 3);
-			eligibleDate.setDate(1);
 
 			if(regDate > arrivalDate && regDate.getMonth() != arrivalDate.getMonth())
 			{
 				eligibleDate.setMonth(regDate.getMonth() + 2);
+				eligibleDate.setDate(1);
+
+				console.log("Registration date: " + regDate);
+
 				if(regDate.getDate() > 15)
 				{
-					eligibleDate.setMonth(regDate.getMonth() + 3);
+					eligibleDate.setMonth(eligibleDate.getMonth() + 1);
+					eligibleDate.setDate(1);
 				}
-				console.log(regDate.getMonth());
 			}
-			resString = "The health plan becomes valid on: " + eligibleDate.toLocaleDateString("en-CA") + "<br>健康计划会在 " + eligibleDate.toLocaleDateString("zh-CN") + " 生效。";
+			else
+			{
+				eligibleDate.setMonth(arrivalDate.getMonth() + 3);
+				eligibleDate.setDate(1);
+				// console.log(eligibleDate);
+			}
+			resString = "The health plan becomes valid on: " + eligibleDate.toLocaleDateString("en-CA") + "<br>太阳卡会在 " + eligibleDate.toLocaleDateString("zh-CN") + " 生效。";
 		}
 
 		$("#result").html(resString);
